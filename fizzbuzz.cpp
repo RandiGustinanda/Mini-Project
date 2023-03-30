ad52392ba86877d212b150213c828a0c2607c7a5
Randi <100740228+RandiGustinanda@users.noreply.github.com>
2023-03-31 00:12:16 +0700

    Add files via upload

---
+++ fizzbuzz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+  for (int i = 1 ; i <= 100; i++){
+    if(i % 3 == 0){
+      std::cout << "fizz\n";
+    }else if(i % 5 == 0){
+      std::cout << "buzz\n";
+    }else if(i % 15 == 0){
+      std::cout << "fizzbuzz";
+    }else{
+      std::cout << i << "\n";
+    }
+  }
+  
+}
